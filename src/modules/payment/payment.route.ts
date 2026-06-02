956b38f01ea41178f730e663d8748b2dae213e06
unknown <sr0589071@gmail.com>
2026-06-02 17:37:55 +0600

    updated PaymentRoutes

--- src/modules/payment/payment.route.ts
+++ src/modules/payment/payment.route.ts
@@ -1,21 +1,3 @@
-// import express from 'express';
-// import { PaymentController } from './payment.controller.js';
-// import { PaymentValidation } from './payment.validation.js';
-// import { validateRequest } from '../../middlewares/validate.middleware.js';
-
-
-// const router = express.Router();
-
-// router.get('/', PaymentController.getAllPayments);
-// router.get('/student/:studentId', PaymentController.getStudentPayments);
-
-// router.post(
-//   '/initiate',
-//   validateRequest(PaymentValidation.createPaymentZodSchema),
-//   PaymentController.createPayment
-// );
-
-// export const PaymentRoutes = router;
 
 import express from 'express';
 import { PaymentController } from './payment.controller.js';
